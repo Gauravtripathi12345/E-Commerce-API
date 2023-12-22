8a86bab32d0d96d9504b60f3b5df0315a90170a4
Gauravtripathi12345 <tripathigaurav38@gmail.com>
2023-12-22 12:30:10 +0530

    Setting up Routes using Router()

---
+++ src/features/product/product.routes.js
@@ -0,0 +1,15 @@
+// Manage routes/paths to ProductController  
+
+// 1 . Import express.
+import express from 'express';
+import ProductController from './product.controller.js';
+
+// 2. Initialize Express router.
+const router = express.Router();
+
+const productController = new ProductController();
+// All the paths to controller methods.
+router.get("/", productController.getAllProducts);
+router.post("/", productController.addProduct);
+
+export default router;
