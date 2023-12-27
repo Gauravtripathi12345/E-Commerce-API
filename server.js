f4b7131ad12a09d99354c92f7a08954733f196a5
Gauravtripathi12345 <tripathigaurav38@gmail.com>
2023-12-27 18:05:50 +0530

    Implementing Basic Authentication

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import express from 'express';
 import productRouter from './src/features/product/product.routes.js';
 import bodyParser from 'body-parser';
 import userRouter from './src/features/user/user.routes.js';
-
+import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 
 // 2. Create Server
 const server = express();
 // server.use(bodyParser.json());
 server.use(express.json());
 // for all requests related to product, redirect to product routes.
-server.use('/api/products', productRouter);
+server.use('/api/products', basicAuthorizer, productRouter);
 server.use('/api/users', userRouter);
 
 
--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -4,14 +4,14 @@ export default class UserController {
 
     signUp(req, res) {
         const { name, email, password, type } = req.body;
-        const user = UserModel.SignUp(name, email, password, type);
+        const user = UserModel.signUp(name, email, password, type);
         console.log("New user signup", user);
         res.status(201).send(user);
     }
 
     signIn(req, res) {
         const { email, password } = req.body;
-        const result = UserModel.SignIn(email, password);
+        const result = UserModel.signIn(email, password);
         console.log("result", result);
         if (!result) {
             return res.status(400).send("Incorrect Credentials");
